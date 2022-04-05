1a190e3a04e63a6ea3411ec982bdcbb781786b55
PocketMiner82 <73847326+PocketMiner82@users.noreply.github.com>
2022-04-05 14:26:07 +0200

    Use moonraker and klipper fork

--- moonraker/components/update_manager/base_config.py
+++ moonraker/components/update_manager/base_config.py
@@ -24,7 +24,7 @@ KLIPPER_DEFAULT_EXEC = os.path.expanduser("~/klippy-env/bin/python")
 
 BASE_CONFIG: Dict[str, Dict[str, str]] = {
     "moonraker": {
-        "origin": "https://github.com/arksine/moonraker.git",
+        "origin": "https://github.com/PocketMiner82/moonraker.git",
         "requirements": "scripts/moonraker-requirements.txt",
         "venv_args": "-p python3",
         "install_script": "scripts/install-moonraker.sh",
@@ -34,8 +34,8 @@ BASE_CONFIG: Dict[str, Dict[str, str]] = {
         "managed_services": "moonraker"
     },
     "klipper": {
-        "moved_origin": "https://github.com/kevinoconnor/klipper.git",
-        "origin": "https://github.com/Klipper3d/klipper.git",
+        #"moved_origin": "https://github.com/kevinoconnor/klipper.git",
+        "origin": "https://github.com/PocketMiner82/klipper.git",
         "requirements": "scripts/klippy-requirements.txt",
         "venv_args": "-p python2",
         "install_script": "scripts/install-octopi.sh",
